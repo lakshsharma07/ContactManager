ef8604cab730dc34282f6315d8f363a3bdfb541f
Lakshya sharma <42237290+lakshsharma07@users.noreply.github.com>
2020-04-21 20:59:45 +0530

    Updates

---
+++ precache-manifest.1df696ad211b94c4a4e8f6acf39832d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4679a8cbefbcc00f8d1914e00d068d70",
+    "url": "/ContactManager/index.html"
+  },
+  {
+    "revision": "ee36d74133314652844c",
+    "url": "/ContactManager/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "ee36d74133314652844c",
+    "url": "/ContactManager/static/js/2.69b0f435.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/ContactManager/static/js/2.69b0f435.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e654efeaffc76f17bca1",
+    "url": "/ContactManager/static/js/main.38880102.chunk.js"
+  },
+  {
+    "revision": "753d30902947cf8ba1dd",
+    "url": "/ContactManager/static/js/runtime-main.7212b41a.js"
+  }
+]);
